27bddb315dcaecea3f6923daa49176441c8253b8
Excellent <standarttechnology8891@gmail.com>
2022-04-21 13:44:08 +0300

    added a starting structure

---
+++ pkg/telegram/bot.go
@@ -0,0 +1,32 @@
+package telegram
+
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"html/template"
+	"log"
+)
+
+type Bot struct {
+	bot         *tgbotapi.BotAPI
+	site        *template.Template
+	redirectUrl string
+}
+
+func NewBot(bot *tgbotapi.BotAPI, site *template.Template, redirectUrl string) *Bot {
+	return &Bot{bot: bot, site: site, redirectUrl: redirectUrl}
+}
+
+func (b *Bot) StartBot() error {
+	log.Printf("Authorized on account #%s name is #%v inline is:%v", b.bot.Self.UserName, b.bot.Self.FirstName, b.bot.Self.SupportsInlineQueries)
+	updates := b.initUpdatesChannel()
+	b.handleUpdates(updates)
+	return nil
+}
+
+func (b *Bot) initUpdatesChannel() tgbotapi.UpdatesChannel {
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+	updates := b.bot.GetUpdatesChan(u)
+
+	return updates
+}
